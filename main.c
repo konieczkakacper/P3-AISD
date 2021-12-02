708f7544eff4600e28a1989e46b97c384aa6c84d
Kacper <73229482+konieczkakacper@users.noreply.github.com>
2021-12-02 19:31:46 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
